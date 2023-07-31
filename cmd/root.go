f49bfe4daa642413ea5bf1b00523a5204e69dc38
ZinedineR <zinedin505@gmail.com>
2023-07-31 12:47:19 +0700

    trying deploy

--- cmd/root.go
+++ cmd/root.go
@@ -3,8 +3,6 @@ package cmd
 import (
 	"os"
 
-	"github.com/joho/godotenv"
-	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 )
@@ -23,14 +21,13 @@ func init() {
 	rootCmd.AddCommand(HttpCmd)
 
 	//load environment variable
-	if err := godotenv.Load(); err != nil {
-		if os.Getenv("APP_ENV") == "development" {
-			logrus.Fatalln("unable to load environment variable", err.Error())
-		} else {
-			logrus.Warningln("Can't find env.file. To use system's env vars for now")
-		}
-
-	}
+	// if err := godotenv.Load(); err != nil {
+	// 	if os.Getenv("APP_ENV") == "development" {
+	// 		logrus.Fatalln("unable to load environment variable", err.Error())
+	// 	} else {
+	// 		logrus.Warningln("Can't find env.file. To use system's env vars for now")
+	// 	}
+	// }
 }
 func Execute() error {
 	cmd, _, err := rootCmd.Find(os.Args[1:])
